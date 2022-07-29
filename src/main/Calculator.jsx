4b56e024c8da6af9ea17c2a5ad49a147fa2b97c1
Fabio Pina <fabio@passwordinfo.com.br>
2022-07-29 00:22:47 -0300

    Build a react calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+import Button from "../components/Button";
+import Display from "../components/Display";
+const initialState = {
+  displayValue: "0",
+  values: [0, 0],
+  operation: null,
+  clearDisplay: false,
+  current: 0,
+};
+export default class Calculator extends Component {
+  state = { ...initialState };
+
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+
+  clearMemory() {
+    this.setState({ ...initialState });
+  }
+
+  setOperation(operation) {
+    const values = [...this.state.values];    
+    const equals = operation === '='
+    var result = 0;
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      if (this.state.operation === "+") {
+        result = values[0] + values[1];
+      }
+      if (this.state.operation === "-") {
+        result = values[0] - values[1];
+      }
+      if (this.state.operation === "*") {
+        result = values[0] * values[1];
+      }
+      if (this.state.operation === "/") {
+        result = values[0] / values[1];
+      }
+      values[0] = result;
+      values[1] = 0;
+      this.setState({
+        displayValue: result,
+        operation: equals ? null : operation,
+        values,
+        clearDisplay: !equals,
+        current: equals ? 0 : 1,
+      });
+    }
+  }
+
+  addDigit(n) {
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === "0" || this.state.clearDisplay;
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, clearDisplay: false });
+
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      console.log(values);
+      this.setState({ values });
+    }
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Display value={this.state.displayValue}></Display>
+        <Button label="AC" click={this.clearMemory} triple />
+        <Button label="/" click={this.setOperation} operation />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
+        <Button label="*" click={this.setOperation} operation />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
+        <Button label="-" click={this.setOperation} operation />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
+        <Button label="+" click={this.setOperation} operation />
+        <Button label="0" click={this.addDigit} double />
+        <Button label="." click={this.addDigit} />
+        <Button label="=" click={this.setOperation} operation />
+      </div>
+    );
+  }
+}
